582f095ea0955766efd9ab03f2d9f71044e69998
deja <deja.greene@tlgcohort.com>
2022-11-14 22:26:03 +0000

    learning about input function

---
+++ deleteme.py
@@ -0,0 +1,18 @@
+ls deleteme.py
+ls deleteme.py#!/bin/rm
+# This script calls on the executable called **rm** (remove), which is
+# installed to the */bin* directory. When this script is run,
+# bash will execute this script with *rm*, which will remove (delete)
+# the script :p
+#!/bin/rm
+# This script calls on the executable called **rm** (remove), which is
+# installed to the */bin* directory. When this script is run,
+# bash will execute this script with *rm*, which will remove (delete)
+# the script :p
+#!/bin/rm
+# This script calls on the executable called **rm** (remove), which is
+# installed to the */bin* directory. When this script is run,
+# bash will execute this script with *rm*, which will remove (delete)
+# the script :p
+
+
